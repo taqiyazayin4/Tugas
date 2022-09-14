b4a225734a4cedb089217eeb5df56bc105184f3a
taqiya.zayin <taqiya.zayin@ui.ac.id>
2022-09-14 22:49:46 +0700

    ver1

--- katalog/views.py
+++ katalog/views.py
@@ -3,11 +3,11 @@ from katalog.models import CatalogItem
 
 # TODO: Create your views here.
 def show_katalog (request):
-    return render(request, "katalog.html")
+    return render(request, "katalog.html", context)
 
 data_barang_katalog = CatalogItem.objects.all()
 context = {
     'list_barang': data_barang_katalog,
     'nama': 'Taqiya Zayin Hanafie',
-    'id' : '2106751335',
+    'id' : '2106751335'
 }
--- project_django/urls.py
+++ project_django/urls.py
@@ -18,8 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('example_app.urls')),
-    path('katalog/', include('katalog.urls')),
+    path('', include('katalog.urls')),
 ]
 
 # TODO: Implement Routings Here
