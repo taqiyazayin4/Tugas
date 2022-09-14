163289fe4910a811a31dca65da53a97aab97a68f
taqiya.zayin <taqiya.zayin@ui.ac.id>
2022-09-14 12:06:07 +0700

    ver1

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+def show_katalog (request):
+    return render(request, "katalog.html")
+
+data_barang_katalog = CatalogItem.objects.all()
+context = {
+    'list_barang': data_barang_katalog,
+    'nama': 'Taqiya Zayin Hanafie',
+    'id' : '2106751335',
+}
